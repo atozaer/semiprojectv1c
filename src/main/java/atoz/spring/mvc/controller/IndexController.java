999c758aab3d530cb67ba0a68e09c0b15b07e5de
atozaer <108923268+atozaer@users.noreply.github.com>
2022-09-28 15:11:16 +0900

    V 1.0.3

--- src/main/java/atoz/spring/mvc/controller/IndexController.java
+++ src/main/java/atoz/spring/mvc/controller/IndexController.java
@@ -1,13 +1,13 @@
 package atoz.spring.mvc.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class IndexController {
 
 
-    @GetMapping("/")
+    @GetMapping("/index")
     public String index() {
 
         return "index";
